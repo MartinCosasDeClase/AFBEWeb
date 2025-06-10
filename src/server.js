5ec2571858e1d335ce21283b759829a3374aa6c0
marti <martincosasdeclase@gmail.com>
2025-06-11 01:09:55 +0200

    commit despliegue a ver si ahora si

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/AFBEWeb')));
+app.use(express.static(path.join(__dirname, 'dist/afbeweb')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/AFBEWeb/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/afbeweb/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
